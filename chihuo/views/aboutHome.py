469ba6137744c8b49dadd2680d42f9b3439cb83c
jcwayd <qq12cvhj@163.com>
2018-04-24 22:22:13 +0800

    添加主页信息推荐关于热门用户的显示的后台逻辑.APP端同步。

--- chihuo/views/aboutHome.py
+++ chihuo/views/aboutHome.py
@@ -1,3 +1,62 @@
-from flask import Blueprint
+from operator import or_
 
-aboutHome = Blueprint('aboutHome',__name__)
+import re
+from flask import Blueprint, json
+
+from chihuo.views import SERVER_IP
+from ..dbModels import user, action, food, share
+from ..dbConnect import db_session
+
+aboutHome = Blueprint('aboutHome', __name__)
+
+
+@aboutHome.route("/getHotUserList", methods=['GET'])
+def getHotUserList():
+    uJsonList = []
+    try:
+        userList = db_session.query(user).all()
+        for u in userList:
+            uJsonList.append(hotUser2Json(u))
+    except Exception, e:
+        print e
+        uJsonList = []
+    print json.dumps(uJsonList)
+    return json.dumps(uJsonList)
+
+
+def hotUser2Json(u):
+    return {
+        "userid": u.userId,
+        "nickname": u.nickName,
+        "imgList": getUserImgs(u.userId)
+    }
+
+
+def getUserImgs(id):
+    imgList = []
+    try:
+        actions = db_session.query(action).filter(action.subjectId == id,
+                                                  or_(action.actionType == 1, action.actionType == 2)).all()
+        print len(actions)
+        if len(actions) > 3:
+            print "???"
+            actions = actions[1:3]
+        for a in actions:
+            if a.actionType == 1:
+                actionob = db_session.query(food).filter(food.foodId == a.objectId).first()
+                jpglist = re.findall(r"/imgsUpload/(.*?)\.jpg", actionob.foodDetail)
+                if jpglist == []:
+                    imgList.append(SERVER_IP + "static/imgsUpload/chihuo.png")
+                else:
+                    imgList.append(SERVER_IP + "static/imgsUpload/" + jpglist[0] + ".jpg")
+            elif a.actionType == 2:
+                actionob = db_session.query(share).filter(share.shareId == a.objectId).first()
+                jpglist = re.findall(r"src=\"(.*?)\.jpg", actionob.shareDetail)
+                if jpglist == []:
+                    imgList.append(SERVER_IP + "static/imgsUpload/chihuo.png")
+                else:
+                    imgList.append(jpglist[0] + ".jpg")
+    except Exception, e:
+        print e
+        imgList = []
+    return imgList
--- runServer.py
+++ runServer.py
@@ -1,13 +1,15 @@
-#coding:utf-8
+# coding:utf-8
 from chihuo import app
 from chihuo.dbConnect import init_db
 from chihuo import models_import_admin
+
 """转换字符，确保中文参数可以传到前端页面,字符编码相关，否则会有中文编码错误。"""
 import sys
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 if __name__ == '__main__':
     init_db()
     models_import_admin()
-    app.run('0.0.0.0',5000,debug = True,threaded=True)
+    app.run('0.0.0.0', 5000, debug=True, threaded=True)
